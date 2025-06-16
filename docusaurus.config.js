824ef82ab76f45b46b4479a6ab6948c6a38d6bd7
Avi Deitcher <avi@deitcher.net>
2025-06-16 16:59:57 +0300

    project page reference
    Signed-off-by: Avi Deitcher <avi@deitcher.net>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://clowder.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/clowder.github.io',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
